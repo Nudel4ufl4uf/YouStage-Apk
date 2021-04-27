a6a7784fed019c8688cbccfe1bf54c6787e610e7
Nudel4ufl4uf <77702798+Nudel4ufl4uf@users.noreply.github.com>
2021-04-27 21:28:50 +0200

    creted Homepage

--- youstage/lib/src/HomeScreen/HomePage.dart
+++ youstage/lib/src/HomeScreen/HomePage.dart
@@ -2,7 +2,68 @@ import 'package:flutter/material.dart';
 
 
 class HomePage extends StatelessWidget {
+  int currentIndex = 0;
+  
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title:Text('Bottom navigation example')
+        ),
+      body: Container(),
+      bottomNavigationBar: BottomNavigationBar( 
+        backgroundColor: Colors.white,
+        currentIndex: currentIndex,
+        type: BottomNavigationBarType.fixed,
+        items: [
+          BottomNavigationBarItem(
+            TextField(
+              decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              hintText: 'Spotlight'
+              ),
+            ),
+            backgroundColor: Colors.pink[400],
+          ),
+          BottomNavigationBarItem(
+            TextField(
+              decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              hintText: 'Kategorien'
+              ),
+            ),
+            backgroundColor: Colors.pink[400],
+          ),
+          BottomNavigationBarItem(
+            TextField(
+              decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              hintText: 'Suche'
+              ),
+            ),
+            backgroundColor: Colors.pink[400],
+          ),
+        ],
+        onTab: (index){
+          setState((){
+            currentIndex = index;
+          });
+
+        }
+      ),
+    );
     }
+  }
+
+class MyApp extends StatefulWidget {
+  @override
+  _State createState() => new _State();
+}
+
+class _State extends State<MyApp> {
+  int count = 0;
+
+  void incrementCounter() {
+    setState(() {
+      count++;
+    });
   }

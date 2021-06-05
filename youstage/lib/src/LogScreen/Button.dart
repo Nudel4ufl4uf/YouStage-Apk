16b8c6a57c02fd73719ba1d870cd98532631e6fe
Nudel4ufl4uf <77702798+Nudel4ufl4uf@users.noreply.github.com>
2021-06-06 00:05:02 +0200

    forgot to save

--- youstage/lib/src/LogScreen/Button.dart
+++ youstage/lib/src/LogScreen/Button.dart
@@ -3,33 +3,33 @@ import 'package:flutter/material.dart';
 import '../../main.dart';
 import 'LogInData.dart';
 
-class Button extends StatelessWidget{
-  
+class Button extends StatelessWidget {
   Button();
-  
-  Widget build (BuildContext context){
-    return Container( 
-      margin: EdgeInsets.only(left: 75,right: 75),
+
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(left: 75, right: 75),
       height: 50,
       width: double.infinity,
-      child: ElevatedButton(  
-                child: Text('Login', style: TextStyle(fontSize: 20.0,fontWeight: FontWeight.bold),),  
-                style: ButtonStyle(
-                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                    RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(35),
-                    side: BorderSide(color: Colors.red)
-                    )
-                  ),
-                  backgroundColor: MaterialStateProperty.all<Color>(Colors.pink[600]),
-              ),  
-                onPressed: () {
-                  Navigator.pushNamed(
-                    context,
-                    '/home',
-                    );
-                },  
-              ),  
-    ); 
+      child: ElevatedButton(
+        child: Text(
+          'Login',
+          style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+        ),
+        style: ButtonStyle(
+          shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+              RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(35),
+                  side: BorderSide(color: Colors.red))),
+          backgroundColor: MaterialStateProperty.all<Color>(Colors.pink[600]),
+        ),
+        onPressed: () {
+          Navigator.pushNamed(
+            context,
+            '/home',
+          );
+        },
+      ),
+    );
   }
 }
